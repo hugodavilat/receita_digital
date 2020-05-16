bebe5cd99fe532a3ac3491c86f380a0aaf3f35ab
Ariel <dsrariel@gmail.com>
2020-05-16 16:15:33 -0300

    Change Patient admin view. Change Patient model required fields.

--- main/admin.py
+++ main/admin.py
@@ -1,9 +1,12 @@
 from django.contrib import admin
 from main import models
 
+class PacienteAdmin(admin.ModelAdmin):
+    list_display = ('id_paciente', 'nome')
+
 admin.site.register(models.Farmacia)
 admin.site.register(models.Medicamento)
 admin.site.register(models.Medico)
-admin.site.register(models.Paciente)
+admin.site.register(models.Paciente, PacienteAdmin)
 admin.site.register(models.Posologia)
 admin.site.register(models.Receita)
--- main/models.py
+++ main/models.py
@@ -33,11 +33,14 @@ class Medico(models.Model):
 
 class Paciente(models.Model):
     id_paciente = models.CharField(max_length=100, primary_key=True, unique=True)
-    nome = models.CharField(max_length=20, blank=False, null=False)
-    data_nascimento = models.DateField(verbose_name='Data de Nascimento')
+    nome = models.CharField(max_length=20, null=False)
+    data_nascimento = models.DateField(verbose_name='Data de Nascimento', blank=True)
     telefone = models.CharField(max_length=20)
-    cpf = models.CharField(max_length=11)
-    email = models.EmailField()
+    cpf = models.CharField(max_length=11, blank=True)
+    email = models.EmailField(blank=True)
+
+    def __str__(self):
+        return self.nome
 
     class Meta:
         db_table = 'paciente'
