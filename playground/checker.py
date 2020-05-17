56bdedc97639044cf2e0785954bd2bf64c4a5d6c
Marco Sousa <mtdsousa@gmail.com>
2020-05-17 15:48:25 -0300

    including more examples and updating requirements

--- playground/checker.py
+++ playground/checker.py
@@ -12,7 +12,8 @@ private_system = PrivateKey(bytes.fromhex(private_system))
 doctors = {
     "doctor1" : "58791eb10d367b8ce8328a2649e87a166fdec7bcc679104ccc7b49295652eb0d",
     "doctor2" : "7417ac098e525b273f70415e5663eb8051a25035d51fb853d5755d4a74bfb800",
-    "doctor3" : "9462126cb2aa6a70204439a7446ed9feac5b0c4cd5415aece163a672f6650040"
+    "doctor3" : "9462126cb2aa6a70204439a7446ed9feac5b0c4cd5415aece163a672f6650040",
+    "doctor4" : "db25731fc67f16dce39d72c5081c688990b4bf45421ed464571d58836b707b00"
 }
 
 challenge = ''.join(random.choice(string.ascii_lowercase + string.digits) for _ in range(7))
