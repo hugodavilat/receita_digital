20163a82a8483a9bf517c70435c4c8607e6ccd48
Marco Sousa <mtdsousa@gmail.com>
2020-05-17 03:01:21 -0300

    adding admin to user_type choices and set user_type as a required field

--- accounts/models.py
+++ accounts/models.py
@@ -5,7 +5,9 @@ class User(AbstractUser):
     USER_TYPE_CHOICES = (
         (1, 'doctor'),
         (2, 'pharmacy'),
-        (3, 'patient')
+        (3, 'patient'),
+        (4, 'admin')
     )
 
     user_type = models.PositiveSmallIntegerField(choices=USER_TYPE_CHOICES)
+    REQUIRED_FIELDS = [ 'user_type' ]
