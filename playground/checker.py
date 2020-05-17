81976d9f8fca0c9eb6d0d94f699a539c714ea373
Marco Sousa <mtdsousa@gmail.com>
2020-05-17 12:37:13 -0300

    trying to perform authentication

---
+++ playground/checker.py
@@ -0,0 +1,29 @@
+import random
+import string
+
+import nacl.utils
+from nacl.public import PrivateKey, PublicKey, Box
+
+# Private-key for the system
+private_system = "d9136c8ba792fef21c9ea11ec80cf3a38cc702e36948ece61c340208b0ac596a"
+private_system = PrivateKey(bytes.fromhex(private_system))
+
+# Public-key stored for doctors
+doctors = {
+    "doctor1" : "58791eb10d367b8ce8328a2649e87a166fdec7bcc679104ccc7b49295652eb0d",
+    "doctor2" : "7417ac098e525b273f70415e5663eb8051a25035d51fb853d5755d4a74bfb800",
+    "doctor3" : "9462126cb2aa6a70204439a7446ed9feac5b0c4cd5415aece163a672f6650040"
+}
+
+challenge = ''.join(random.choice(string.ascii_lowercase + string.digits) for _ in range(32))
+doc = input("Which doctor are you trying to authenticate (doctor1, doctor2 or doctor3)? ")
+message = input("Please, tell us the result for the challenge '{}': ".format(challenge))
+
+message = bytes.fromhex(message)
+print(len(message))
+public_doctor = doctors[doc]
+public_doctor = PublicKey(bytes.fromhex(public_doctor))
+
+box = Box(private_system, public_doctor)
+plaintext = box.decrypt(message)
+print(plaintext.decode('utf-8'))
